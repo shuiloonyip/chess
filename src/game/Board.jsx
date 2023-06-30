0eaff7370281905dc793d7024a9585d5e00e9989
shuiloonyip <97403751+shuiloonyip@users.noreply.github.com>
2023-07-01 00:10:24 +0100

    Display board

--- src/game/Board.jsx
+++ src/game/Board.jsx
@@ -1,198 +1,94 @@
-// import { Chess } from "chess.js";
 import Tile from "./Tile";
+import "./Board.css";
 
 function Board() {
-  // const chess = new Chess();
-
-  // let board = chess.board();
-
   const DUMMYDATA = [
     [
-      {
-        square: "a8",
-        type: "r",
-        color: "b",
-      },
-      {
-        square: "b8",
-        type: "n",
-        color: "b",
-      },
-      {
-        square: "c8",
-        type: "b",
-        color: "b",
-      },
-      {
-        square: "d8",
-        type: "q",
-        color: "b",
-      },
-      {
-        square: "e8",
-        type: "k",
-        color: "b",
-      },
-      {
-        square: "f8",
-        type: "b",
-        color: "b",
-      },
-      {
-        square: "g8",
-        type: "n",
-        color: "b",
-      },
-      {
-        square: "h8",
-        type: "r",
-        color: "b",
-      },
+      { square: "a8", type: "r", color: "b" },
+      { square: "b8", type: "n", color: "b" },
+      { square: "c8", type: "b", color: "b" },
+      { square: "d8", type: "q", color: "b" },
+      { square: "e8", type: "k", color: "b" },
+      { square: "f8", type: "b", color: "b" },
+      { square: "g8", type: "n", color: "b" },
+      { square: "h8", type: "r", color: "b" },
     ],
     [
-      {
-        square: "a7",
-        type: "p",
-        color: "b",
-      },
-      {
-        square: "b7",
-        type: "p",
-        color: "b",
-      },
-      {
-        square: "c7",
-        type: "p",
-        color: "b",
-      },
-      {
-        square: "d7",
-        type: "p",
-        color: "b",
-      },
-      {
-        square: "e7",
-        type: "p",
-        color: "b",
-      },
-      {
-        square: "f7",
-        type: "p",
-        color: "b",
-      },
-      {
-        square: "g7",
-        type: "p",
-        color: "b",
-      },
-      {
-        square: "h7",
-        type: "p",
-        color: "b",
-      },
+      { square: "a7", type: "p", color: "b" },
+      { square: "b7", type: "p", color: "b" },
+      { square: "c7", type: "p", color: "b" },
+      { square: "d7", type: "p", color: "b" },
+      { square: "e7", type: "p", color: "b" },
+      { square: "f7", type: "p", color: "b" },
+      { square: "g7", type: "p", color: "b" },
+      { square: "h7", type: "p", color: "b" },
     ],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [
-      {
-        square: "a2",
-        type: "p",
-        color: "w",
-      },
-      {
-        square: "b2",
-        type: "p",
-        color: "w",
-      },
-      {
-        square: "c2",
-        type: "p",
-        color: "w",
-      },
-      {
-        square: "d2",
-        type: "p",
-        color: "w",
-      },
-      {
-        square: "e2",
-        type: "p",
-        color: "w",
-      },
-      {
-        square: "f2",
-        type: "p",
-        color: "w",
-      },
-      {
-        square: "g2",
-        type: "p",
-        color: "w",
-      },
-      {
-        square: "h2",
-        type: "p",
-        color: "w",
-      },
+      { square: "a2", type: "p", color: "w" },
+      { square: "b2", type: "p", color: "w" },
+      { square: "c2", type: "p", color: "w" },
+      { square: "d2", type: "p", color: "w" },
+      { square: "e2", type: "p", color: "w" },
+      { square: "f2", type: "p", color: "w" },
+      { square: "g2", type: "p", color: "w" },
+      { square: "h2", type: "p", color: "w" },
     ],
     [
-      {
-        square: "a1",
-        type: "r",
-        color: "w",
-      },
-      {
-        square: "b1",
-        type: "n",
-        color: "w",
-      },
-      {
-        square: "c1",
-        type: "b",
-        color: "w",
-      },
-      {
-        square: "d1",
-        type: "q",
-        color: "w",
-      },
-      {
-        square: "e1",
-        type: "k",
-        color: "w",
-      },
-      {
-        square: "f1",
-        type: "b",
-        color: "w",
-      },
-      {
-        square: "g1",
-        type: "n",
-        color: "w",
-      },
-      {
-        square: "h1",
-        type: "r",
-        color: "w",
-      },
+      { square: "a1", type: "r", color: "w" },
+      { square: "b1", type: "n", color: "w" },
+      { square: "c1", type: "b", color: "w" },
+      { square: "d1", type: "q", color: "w" },
+      { square: "e1", type: "k", color: "w" },
+      { square: "f1", type: "b", color: "w" },
+      { square: "g1", type: "n", color: "w" },
+      { square: "h1", type: "r", color: "w" },
     ],
   ];
 
-  // prettier-ignore
-  return <div className="grid grid-cols-8 grid-rows-8">
-  {[
-    [<Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />],
-    [<Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />],
-    [<Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />],
-    [<Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />],
-    [<Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />],
-    [<Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />],
-    [<Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />],
-    [<Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />, <Tile />],
-  ]}</div>;
+  const rowLabel = ["8", "7", "6", "5", "4", "3", "2", "1"];
+  const columnLabel = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+  const boardArr = [];
+
+  for (let i = 0; i < rowLabel.length; i++) {
+    const rowArr = [];
+    for (let j = 0; j < columnLabel.length; j++) {
+      if ((i + j) % 2 === 0) {
+        rowArr.push({
+          square: `${columnLabel[j]}${rowLabel[i]}`,
+          tileColor: "white",
+        });
+      } else {
+        rowArr.push({
+          square: `${columnLabel[j]}${rowLabel[i]}`,
+          tileColor: "black",
+        });
+      }
+    }
+    boardArr.push(rowArr);
+  }
+
+  const boardRender = boardArr.map((row, index) => {
+    return (
+      <div className="row" key={rowLabel[index]}>
+        {row.map((tile) => {
+          return (
+            <Tile
+              key={tile.square}
+              tileColor={tile.tileColor}
+              square={tile.square}
+            />
+          );
+        })}
+      </div>
+    );
+  });
+
+  return <div className="board">{boardRender}</div>;
 }
 
 export default Board;
--- src/game/Tile.jsx
+++ src/game/Tile.jsx
@@ -1,6 +1,12 @@
 import "./Tile.css";
-function Tiles() {
-  return <div className="tile"></div>;
+
+function Tiles(props) {
+  const { tileColor } = props;
+  const { square } = props;
+
+  const newClassName = tileColor + " tile";
+
+  return <div className={newClassName}>{square}</div>;
 }
 
 export default Tiles;
