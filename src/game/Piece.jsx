f034298d18f7dec2c4b33580c5c8533a31ad961f
shuiloonyip <97403751+shuiloonyip@users.noreply.github.com>
2023-07-02 20:29:39 +0100

    Show assets on board

--- src/game/Piece.jsx
+++ src/game/Piece.jsx
@@ -1,9 +1,43 @@
+import bb from "../assets/bb.svg";
+import bk from "../assets/bk.svg";
+import bn from "../assets/bn.svg";
+import bp from "../assets/bp.svg";
+import bq from "../assets/bq.svg";
+import br from "../assets/br.svg";
+import wb from "../assets/wb.svg";
+import wk from "../assets/wk.svg";
+import wn from "../assets/wn.svg";
+import wp from "../assets/wp.svg";
+import wq from "../assets/wq.svg";
+import wr from "../assets/wr.svg";
 import "./Piece.css";
+
+const chessPieces = {
+  bb,
+  bk,
+  bn,
+  bp,
+  bq,
+  br,
+  wb,
+  wk,
+  wn,
+  wp,
+  wq,
+  wr,
+};
 
 function Piece(props) {
   const { piece } = props;
 
-  return <div className={"piece " + piece.color}>{piece.type}</div>;
+  return (
+    <div className={"piece"}>
+      <img
+        src={chessPieces[`${piece.color + piece.type}`]}
+        alt={`${piece.color + piece.type}`}
+      ></img>
+    </div>
+  );
 }
 
 export default Piece;
